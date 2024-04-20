65eeda202ceec4ef1330aa6383ecd5d0e95976c5
EliGandin <git>
2024-04-20 22:33:36 +0300

    fixed global styling error

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -6,12 +6,12 @@ const About = () => {
     <div className={styles.container}>
       <Icon name="lightbulb outline" size="huge" className={styles.icon} />
       <header>
-        <h1>About Taf</h1>
+        <h1 className={styles.h1}>About Taf</h1>
       </header>
       <main>
         <section>
-          <h2>Our Mission</h2>
-          <p>
+          <h2 className={styles.h2}>Our Mission</h2>
+          <p className={styles.p}>
             At Taf, we are dedicated to simplifying the lives of parents by
             providing a platform that helps them find the perfect babysitter for
             their children. Our mission is to empower parents to manage their
@@ -20,8 +20,8 @@ const About = () => {
           </p>
         </section>
         <section>
-          <h2>Our Approach</h2>
-          <p>
+          <h2 className={styles.h2}>Our Approach</h2>
+          <p className={styles.p}>
             At Taf, we prioritize parents. We understand the challenges and
             responsibilities that come with raising a family, which is why our
             platform is designed with a parents-first approach. We are committed
@@ -30,27 +30,27 @@ const About = () => {
           </p>
         </section>
         <section>
-          <h2>Our Values</h2>
-          <ul>
-            <li>
+          <h2 className={styles.h2}>Our Values</h2>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
               <Icon name="handshake" />
               <strong>Trust:</strong> We prioritize trust and reliability in
               every interaction, ensuring peace of mind for parents and
               caregivers alike.
             </li>
-            <li>
+            <li className={styles.li}>
               <Icon name="chart line" />
               <strong>Quality:</strong> We uphold high standards of quality in
               our services, connecting families with experienced and caring
               babysitters.
             </li>
-            <li>
+            <li className={styles.li}>
               <Icon name="home" />
-              <strong>Community:</strong> We foster a supportive community where
-              parents can share experiences, resources, and advice, creating a
-              network of support.
+              <strong className={styles.strong}>Community:</strong> We foster a
+              supportive community where parents can share experiences,
+              resources, and advice, creating a network of support.
             </li>
-            <li>
+            <li className={styles.li}>
               <Icon name="clock outline" />
               <strong>Flexibility:</strong> We understand that every family is
               unique, so we offer flexible solutions to accommodate varying
