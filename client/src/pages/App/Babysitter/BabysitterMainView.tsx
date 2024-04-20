c5a7b6b7aab3d665203daf9feaeca7235b255554
EliGandin <87780727+EliGandin@users.noreply.github.com>
2024-04-20 22:24:29 +0300

    added boxing elements and better description to main views (#11)
    Co-authored-by: EliGandin <git>

--- client/src/pages/App/Babysitter/BabysitterMainView.tsx
+++ client/src/pages/App/Babysitter/BabysitterMainView.tsx
@@ -1,27 +1,33 @@
-import { Image } from "semantic-ui-react";
+import { Icon, Image } from "semantic-ui-react";
 
 import styles from "./Babysitter.module.css";
 
 const BabysitterMainView = () => {
   return (
-    <div className={styles.container}>
-      <h1>Hello, PLACEHOLDER</h1>
-      <div className={styles.main}>
-        <div className={styles.analytics}>
-          <h2>Analytics</h2>
-          <p>X People viewed your profile</p>
-          <p>You have X recommendations</p>
-          <p>STARS</p>
+    <>
+      <div className={styles.iconContainer}>
+        <Icon name="address card outline" size="huge" className={styles.icon} />
+        <p>Here you can see your analytics</p>
+      </div>
+      <h1 className={styles.h1}>Hello, PLACEHOLDER</h1>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <div className={styles.analytics}>
+            <h2>Analytics</h2>
+            <p>X People viewed your profile</p>
+            <p>You have X recommendations</p>
+            <p>STARS</p>
+          </div>
+          <Image src="/babysitter.svg" size="small" />
         </div>
-        <Image src="/babysitter.svg" size="small" />
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdecJ73IuzOaZdrDLa0aP8eAPc0VKJYuhI72hhEVEzeQ&s"
+            size="medium"
+          />
+        </div>
       </div>
-      <div className={styles.imageContainer}>
-        <Image
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdecJ73IuzOaZdrDLa0aP8eAPc0VKJYuhI72hhEVEzeQ&s"
-          size="medium"
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
--- client/src/pages/App/Moderator/ModeratorMainView.tsx
+++ client/src/pages/App/Moderator/ModeratorMainView.tsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
+import { Icon } from "semantic-ui-react";
+
 import CardsView from "../../../ui/CardsView/CardsView";
+import styles from "./Moderator.module.css";
 
 const ModeratorMainView = () => {
   const [data, setData] = useState([0, 1, 2, 3, 4]);
 
-  return <CardsView data={data} />;
+  return (
+    <>
+      <div className={styles.iconContainer}>
+        <Icon name="address card outline" size="huge" className={styles.icon} />
+        <p>Here you can see the accounts waiting on action</p>
+      </div>
+      <h1 className={styles.h1}>Hello, PLACEHOLDER</h1>
+      <CardsView data={data} />
+    </>
+  );
 };
 
 export default ModeratorMainView;
