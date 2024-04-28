fbcbbf9ee7343d6815ca814ee7d028cc82113b81
idosh <idoshziv@gmail.com>
2024-04-28 14:23:50 +0300

    fixing lint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,6 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactHooks from "eslint-plugin-react-hooks"
 
 
 export default [
@@ -9,4 +11,30 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  {
+    plugins: {
+      "react-refresh": reactRefresh,
+      "react-hooks": reactHooks
+    },
+  },
+  {
+    ignores: ["dist/*", ".eslintrc.cjs"],
+  },
+  {
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
+
+    },
+  },
+  {
+    languageOptions: {
+      parser: tseslint.parser
+    }
+  }
 ];
