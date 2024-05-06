fb20a7be94a32bb342b64b9da1fbd1a369ba0aa3
idosh <idoshziv@gmail.com>
2024-05-07 00:01:53 +0300

    sample routes

--- server/routes/api/recommendations/recommendationsRoutes.ts
+++ server/routes/api/recommendations/recommendationsRoutes.ts
@@ -4,4 +4,20 @@ const recommendationsRouter = Router();
 
 const handler = new Handler();
 
+recommendationsRouter.get("/", (request, response) => {
+    console.log("get recommendations preview request")
+    response.send("WIP")
+});
+recommendationsRouter.get("/:babysitter", (request, response) => {
+    console.log("get all " + request.params.babysitter  +  "'s received recommendation request")
+    response.send("WIP babysitter")
+});
+recommendationsRouter.get("/:parent", (request, response) => {
+    console.log("get all  " + request.params.parent  +  "'s given recommendation request")
+    response.send("WIP parent")
+});
+recommendationsRouter.get("/:parent/:babysitter", (request, response) => {
+    console.log("get recommendations by " + request.params.parent + " for: " + request.params.babysitter)
+    response.send("WIP babysitter parent")
+});
 export default recommendationsRouter;
