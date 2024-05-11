f7628e1d24e2eaf6669f11091ec66615bdf952ed
EliGandin <87780727+EliGandin@users.noreply.github.com>
2024-05-11 17:54:33 +0300

    Navbar login (#35)
    * setup subbracnch
    
    * styled navbar for logged in users
    
    ---------
    
    Co-authored-by: EliGandin <git>

--- client/src/ui/Navbar/Navbar.tsx
+++ client/src/ui/Navbar/Navbar.tsx
@@ -1,30 +1,50 @@
-import { Menu, MenuItem, Button } from "semantic-ui-react";
+import { Menu, MenuItem, Button, Image, MenuMenu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
 import styles from "./NavigationBar.module.css";
+import { userState } from "../../state/atoms/userAtom";
 
 function NavigationBar() {
+  const user = useRecoilValue(userState);
+  const loggedIn = user.id ?? false;
+
   return (
-    <Menu inverted pointing className={styles.navbar}>
-      <MenuItem as={Link} to="/" name="home">
-        Home
+    <Menu inverted className={styles.navbar}>
+      <MenuItem>
+        <Image src="taf-03.svg" className={styles.logo} />
       </MenuItem>
 
-      <MenuItem as={Link} to="/about" name="about">
-        About
-      </MenuItem>
+      <MenuMenu>
+        <MenuItem as={Link} to="/" name="home">
+          Home
+        </MenuItem>
 
-      <MenuItem as={Link} to="/contact" name="contact">
-        Contact
-      </MenuItem>
+        <MenuItem as={Link} to="/about" name="about">
+          About
+        </MenuItem>
 
-      <MenuItem as={Link} to="/signup" name="signup">
-        <Button primary>Sign up</Button>
-      </MenuItem>
+        <MenuItem as={Link} to="/contact" name="contact">
+          Contact
+        </MenuItem>
 
-      <MenuItem as={Link} to="/login" position="right">
-        <Button>Login</Button>
-      </MenuItem>
+        {!loggedIn && (
+          <MenuItem as={Link} to="/signup" name="signup">
+            <Button primary>Sign up</Button>
+          </MenuItem>
+        )}
+      </MenuMenu>
+
+      {!loggedIn ? (
+        <MenuItem as={Link} to="/login" position="right">
+          <Button>Login</Button>
+        </MenuItem>
+      ) : (
+        <MenuItem position="right">
+          {/* //TODO: once get profile is ready */}
+          <Button primary>{`Hello ${user.name}`}</Button>
+        </MenuItem>
+      )}
     </Menu>
   );
 }
