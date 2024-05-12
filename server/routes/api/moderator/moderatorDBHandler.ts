f47393189f875da818ad40179050e12595203271
EliGandin <git>
2024-05-12 17:50:24 +0300

    added end_timestamp to query conditions for active users

--- server/routes/api/moderator/moderatorDBHandler.ts
+++ server/routes/api/moderator/moderatorDBHandler.ts
@@ -5,9 +5,13 @@ export default class DBHandler {
   async getAllUsers(): Promise<User[]> {
     const usersQuery = `SELECT babysitter_id AS id, babysitter_name AS name, email, gender, 'babysitter' AS role
                         FROM babysitters
-                        UNION SELECT parent_id AS id, parent_name AS name, email, gender, 'parent' AS role
+                        WHERE end_timestamp = 9999999999
+                        UNION 
+                        SELECT parent_id AS id, parent_name AS name, email, gender, 'parent' AS role
                         FROM parents
-     `;
+                        WHERE end_timestamp = 9999999999
+                        `;
+
     const allUsers = await db.query(usersQuery);
     return allUsers.rows;
   }
