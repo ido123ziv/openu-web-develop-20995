e5448818ba5a86acb9c4d5c6c35063924e55090b
EliGandin <git>
2024-05-15 16:20:11 +0300

    work on allUsers query for moderator view

--- server/routes/api/moderator/moderatorDBHandler.ts
+++ server/routes/api/moderator/moderatorDBHandler.ts
@@ -3,11 +3,29 @@ import { User } from "./moderatorTypes";
 
 export default class DBHandler {
   async getAllUsers(): Promise<User[]> {
-    const usersQuery = `SELECT babysitter_id AS id, babysitter_name AS name, email, gender, 'babysitter' AS role
+    const usersQuery = `SELECT babysitter_id AS id, 
+                              babysitter_name AS name, 
+                              email, 
+                              gender, 
+                              age, 
+                              phone_number AS phoneNumber,
+                              experience,
+                              image_string AS imageString,
+                              comments,
+                              'babysitter' AS role
                         FROM babysitters
                         WHERE end_timestamp = 9999999999
                         UNION 
-                        SELECT parent_id AS id, parent_name AS name, email, gender, 'parent' AS role
+                        SELECT parent_id AS id,
+                              parent_name AS name, 
+                              email, 
+                              gender, 
+                              phone_number AS phoneNumber, 
+                              min_kid_age AS minKidAge,
+                              max_kid_age AS maxKidAge, 
+                              num_of_kids AS numOfKids, 
+                              comments, 
+                              'parent' AS role
                         FROM parents
                         WHERE end_timestamp = 9999999999
                         `;
--- server/routes/api/moderator/moderatorTypes.ts
+++ server/routes/api/moderator/moderatorTypes.ts
@@ -2,6 +2,14 @@ export type User = {
   id: number;
   name: string;
   email: string;
+  phoneNumber: string;
   gender: string;
   role: string;
+  age?: number;
+  experience?: string;
+  imageString?: string;
+  comments?: string;
+  minKidAge?: number;
+  maxKidAge?: number;
+  numOfKids?: number;
 };
