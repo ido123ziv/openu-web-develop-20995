a2cf8901fb3f7bb233b169dffd9daa3e30c12f6c
EliGandin <git>
2024-05-16 15:27:27 +0300

    fixed moderatorGetAllUsers DBHandler

--- server/routes/api/moderator/moderatorDBHandler.ts
+++ server/routes/api/moderator/moderatorDBHandler.ts
@@ -4,34 +4,35 @@ import { User } from "./moderatorTypes";
 
 export default class DBHandler {
   async getAllUsers(): Promise<User[]> {
-    const usersQuery = `SELECT babysitter_id AS id, 
-                              babysitter_name AS name, 
-                              email, 
-                              gender, 
-                              age, 
-                              phone_number AS phoneNumber,
-                              experience,
-                              image_string AS imageString,
-                              comments,
-                              'babysitter' AS role
-                        FROM babysitters
-                        WHERE end_timestamp = $1
-                        UNION 
-                        SELECT parent_id AS id,
-                              parent_name AS name, 
-                              email, 
-                              gender, 
-                              phone_number AS phoneNumber, 
-                              min_kid_age AS minKidAge,
-                              max_kid_age AS maxKidAge, 
-                              num_of_kids AS numOfKids, 
-                              comments, 
-                              'parent' AS role
+    const parentQuery = `SELECT parent_id AS id,
+                                parent_name AS name, 
+                                email, 
+                                gender, 
+                                phone_number AS phoneNumber, 
+                                min_kid_age AS minKidAge,
+                                max_kid_age AS maxKidAge, 
+                                num_of_kids AS numOfKids, 
+                                comments, 
+                                'parent' AS role
                         FROM parents
-                        WHERE end_timestamp = $1
-                        `;
+                        WHERE end_timestamp = $1`;
 
-    const allUsers = await db.query(usersQuery, [END_TIMESTAMP]);
-    return allUsers.rows;
+    const babysitterQuery = `SELECT babysitter_id AS id, 
+                                  babysitter_name AS name, 
+                                  email, 
+                                  gender, 
+                                  age, 
+                                  phone_number AS phoneNumber,
+                                  experience,
+                                  image_string AS imageString,
+                                  comments,
+                                  'babysitter' AS role
+                          FROM babysitters
+                          WHERE end_timestamp = $1`;
+
+    const parents = await db.query(parentQuery, [END_TIMESTAMP]);
+    const babysitters = await db.query(babysitterQuery, [END_TIMESTAMP]);
+
+    return [...parents.rows, ...babysitters.rows];
   }
 }
--- server/routes/api/moderator/moderatorTypes.ts
+++ server/routes/api/moderator/moderatorTypes.ts
@@ -2,14 +2,16 @@ export type User = {
   id: number;
   name: string;
   email: string;
-  phoneNumber: string;
   gender: string;
-  role: string;
-  age?: number;
-  experience?: string;
+  phoneNumber: string;
+  comments: string;
+  role: "babysitter" | "parent";
+
   imageString?: string;
-  comments?: string;
   minKidAge?: number;
   maxKidAge?: number;
   numOfKids?: number;
+
+  age?: number;
+  experience?: string;
 };
