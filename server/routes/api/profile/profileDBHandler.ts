59b1af2079dee54aa4b2bae96a1a1d6bbb1e6b90
idosh <idoshziv@gmail.com>
2024-05-17 15:20:45 +0300

    [pr comments] taking only columns I need

--- server/routes/api/profile/profileDBHandler.ts
+++ server/routes/api/profile/profileDBHandler.ts
@@ -5,7 +5,16 @@ import { ParentProfile,BabysitterProfile } from "./profileTypes";
 
 export default class DBHandler {
     async getBabySitterProfile(babysitterId: number): Promise<BabysitterProfile[]> {
-        const babysitterQuery = `SELECT * 
+        const babysitterQuery = `SELECT name,
+                                 email,
+                                 password,
+                                 city,
+                                 street,
+                                 gender,
+                                 experience,
+                                 age,
+                                 phone_number AS "phoneNumber",
+                                 comments,
                                  FROM babysitters
                                  WHERE babysitter_id = ($1)
                                  AND end_timestamp = ($2)`;
@@ -14,7 +23,17 @@ export default class DBHandler {
     }
 
     async getParentProfile(parentId: number): Promise<ParentProfile[]> {
-        const parentQuery = `SELECT * 
+        const parentQuery = `SELECT name,
+                             email,
+                             password,
+                             city,
+                             street,
+                             gender,
+                             phone_number AS "phoneNumber",
+                             min_kid_age AS "minKidAge",
+                             max_kid_age AS "maxKidAge",
+                             num_of_kids AS "numOfKids",
+                             comments, 
                              FROM parents 
                              WHERE parent_id = ($1)
                              AND end_timestamp = ($2)`;
