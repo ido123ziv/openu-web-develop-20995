20ad63af1b749733d1eac70d92c7c1ad8a9259b2
liela9 <l.aharoni98@gmail.com>
2024-05-19 20:22:07 +0300

    fix more errors!

--- server/utils/global/globals.ts
+++ server/utils/global/globals.ts
@@ -1,15 +1,3 @@
-import { Request } from "express";
-import { validationResult } from "express-validator";
-
 export const END_TIMESTAMP = 9999999999;
 export const BABYSITTER_INVALID_INPUT_ERROR = "babysitterId must be provided and a number";
 export const PARENT_INVALID_INPUT_ERROR = "parentId must be provided and a number";
-
-
-export function reqUserValidation(req: Request, data: any[]) {
-    if (data.length === 0) {
-        return { isValid: false, message: 'Incorrect id' }
-    }
-
-    return { isValid: true, message: 'Request data is valid' }
-}
