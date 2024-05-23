fc3eeaea5a7c3dbe0a1e4776fbce15d62ee82f93
EliGandin <git>
2024-05-23 12:01:32 +0300

    fixed numViewed bug

--- server/routes/api/babysitter/babysitterDBHandler.ts
+++ server/routes/api/babysitter/babysitterDBHandler.ts
@@ -1,12 +1,12 @@
 import db from "../../../utils/db/db";
 
 export default class DBHandler {
-  async getNumWatchedProfile(babysitterId: number): Promise<number> {
+  async getNumWatchedProfile(babysitterId: number): Promise<string> {
     const query = `SELECT COUNT(*)
                     FROM parents_babysitters_interactions
                     WHERE babysitter_id = $1`;
 
     const numWatched = await db.query(query, [babysitterId]);
-    return numWatched.rows.length;
+    return numWatched.rows[0].count;
   }
 }
