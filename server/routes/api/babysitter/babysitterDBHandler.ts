ade05c8dda3706d08ce83c7d50c8412d63e14d6e
EliGandin <git>
2024-05-24 16:26:45 +0300

    added aliasing

--- server/routes/api/babysitter/babysitterDBHandler.ts
+++ server/routes/api/babysitter/babysitterDBHandler.ts
@@ -2,11 +2,11 @@ import db from "../../../utils/db/db";
 
 export default class DBHandler {
   async getNumWatchedProfile(babysitterId: number): Promise<string> {
-    const query = `SELECT COUNT(*)
+    const query = `SELECT COUNT(*) AS amount
                     FROM parents_babysitters_interactions
                     WHERE babysitter_id = $1`;
 
     const numWatched = await db.query(query, [babysitterId]);
-    return numWatched.rows[0].count;
+    return numWatched.rows[0].amount;
   }
 }
