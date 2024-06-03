bfcd3643030c46650323dcf5b88a5010e1a4af6f
liela9 <72128162+liela9@users.noreply.github.com>
2024-06-03 11:28:51 +0300

    Update unit-test.sh

--- test/unit-test.sh
+++ test/unit-test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# post contact request
 curl --location 'http://localhost:3000/contact' \
 --header 'Content-Type: application/json' \
 --data-raw '{
@@ -7,3 +9,30 @@ curl --location 'http://localhost:3000/contact' \
     "title": "hello",
     "message": "world"
 }'
+
+# edit contact request status
+curl --location --request PUT 'http://localhost:3000/api/moderator/editContactRequestStatus/1' \
+--header 'Content-Type: application/json' \
+--data '{
+    "status": "done"
+}'
+
+# update babysitter
+curl --location --request PUT 'http://localhost:3000/api/profile/babysitter/update/2' \
+--header 'Content-Type: application/json' \
+--data '{
+    "city": "Haifa"
+}'
+
+# update parent
+curl --location --request PUT 'http://localhost:3000/api/profile/parent/update/5' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "Ivgeny11"
+}'
+
+# delete babysitter
+curl --location --request PUT 'http://localhost:3000/api/delete/babysitter/3'
+
+# delete parent
+curl --location --request PUT 'http://localhost:3000/api/delete/parent/3'
