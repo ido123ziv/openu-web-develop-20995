dcc111d722e3e9267ed7140fea0608023284367f
EliGandin <git>
2024-06-08 22:43:53 +0300

    set up route for frontend pieChart

--- server/routes/api/babysitter/babysitterDBHandler.ts
+++ server/routes/api/babysitter/babysitterDBHandler.ts
@@ -1,5 +1,6 @@
 import db from "../../../utils/db/db";
 import { END_TIMESTAMP } from "../../../utils/global/globals";
+import { interactionsData } from "./babysitterTypes";
 
 export default class DBHandler {
   async getBabysitter(babysitterId: number): Promise<number> {
@@ -19,5 +20,17 @@ export default class DBHandler {
 
     const numWatched = await db.query(query, [babysitterId]);
     return numWatched.rows[0].amount;
+  }
+
+  async getInteractionsData(id: number): Promise<interactionsData> {
+    const query = `SELECT COUNT(*) AS "totalCount",
+                          SUM(CASE WHEN contacted = TRUE THEN 1 ELSE 0 END) AS contacted,
+                          SUM(CASE WHEN worked_with = TRUE THEN 1 ELSE 0 END) AS "workedWith"
+                  FROM parents_babysitters_interactions
+                  WHERE babysitter_id = $1
+                  GROUP BY babysitter_id`;
+
+    const { rows } = await db.query(query, [id]);
+    return rows[0];
   }
 }
--- server/routes/api/babysitter/babysitterRoutes.ts
+++ server/routes/api/babysitter/babysitterRoutes.ts
@@ -24,7 +24,7 @@ babysitterRouter.get(
 
       const { babysitterId } = req.params;
 
-      const validation =await handler.userValidation(Number(babysitterId));
+      const validation = await handler.userValidation(Number(babysitterId));
       if (!validation.isValid) {
         return res.status(400).json({ error: validation.message });
       }
@@ -32,6 +32,37 @@ babysitterRouter.get(
       const numOfViews = await handler.numOfViews(Number(babysitterId));
 
       res.status(200).json({ numOfViews });
+    } catch (e) {
+      console.log(
+        `Error message: ${req.body.id}: ${(e as Error).message}\n${
+          (e as Error).stack
+        }`
+      );
+      return res.status(500).end();
+    }
+  }
+);
+
+babysitterRouter.get(
+  "/interactions/:id",
+  [param("id").notEmpty().isNumeric().withMessage("Invalid Input")],
+  async (req: Request, res: Response) => {
+    try {
+      const fieldValidationResult = validationResult(req);
+      if (!fieldValidationResult.isEmpty()) {
+        return res.status(400).json({
+          message: fieldValidationResult
+            .array()
+            .map((item) => item.msg)
+            .join(" "),
+        });
+      }
+
+      const { id } = req.params;
+
+      const data = await handler.getInteractions(Number(id));
+
+      return res.status(200).json({ data });
     } catch (e) {
       console.log(
         `Error message: ${req.body.id}: ${(e as Error).message}\n${
