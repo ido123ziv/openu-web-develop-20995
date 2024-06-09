72cc4e5a006a5db80c97ad6959bb9f5ade50666e
EliGandin <git>
2024-06-09 08:29:48 +0300

    refactored routes

--- server/routes/api/babysitter/babysitterRoutes.ts
+++ server/routes/api/babysitter/babysitterRoutes.ts
@@ -7,41 +7,41 @@ const babysitterRouter = Router();
 
 const handler = new Handler();
 
-babysitterRouter.get(
-  "/:babysitterId",
-  [param("babysitterId").notEmpty().isNumeric().withMessage("Invalid Input")],
-  async (req: Request, res: Response) => {
-    try {
-      const fieldValidationResult = validationResult(req);
-      if (!fieldValidationResult.isEmpty()) {
-        return res.status(400).json({
-          message: fieldValidationResult
-            .array()
-            .map((item) => item.msg)
-            .join(" "),
-        });
-      }
+// babysitterRouter.get(
+//   "/:babysitterId",
+//   [param("babysitterId").notEmpty().isNumeric().withMessage("Invalid Input")],
+//   async (req: Request, res: Response) => {
+//     try {
+//       const fieldValidationResult = validationResult(req);
+//       if (!fieldValidationResult.isEmpty()) {
+//         return res.status(400).json({
+//           message: fieldValidationResult
+//             .array()
+//             .map((item) => item.msg)
+//             .join(" "),
+//         });
+//       }
 
-      const { babysitterId } = req.params;
+//       const { babysitterId } = req.params;
 
-      const validation = await handler.userValidation(Number(babysitterId));
-      if (!validation.isValid) {
-        return res.status(400).json({ error: validation.message });
-      }
+//       const validation = await handler.userValidation(Number(babysitterId));
+//       if (!validation.isValid) {
+//         return res.status(400).json({ error: validation.message });
+//       }
 
-      const numOfViews = await handler.numOfViews(Number(babysitterId));
+//       const numOfViews = await handler.numOfViews(Number(babysitterId));
 
-      res.status(200).json({ numOfViews });
-    } catch (e) {
-      console.log(
-        `Error message: ${req.body.id}: ${(e as Error).message}\n${
-          (e as Error).stack
-        }`
-      );
-      return res.status(500).end();
-    }
-  }
-);
+//       res.status(200).json({ numOfViews });
+//     } catch (e) {
+//       console.log(
+//         `Error message: ${req.body.id}: ${(e as Error).message}\n${
+//           (e as Error).stack
+//         }`
+//       );
+//       return res.status(500).end();
+//     }
+//   }
+// );
 
 babysitterRouter.get(
   "/interactions/:id",
