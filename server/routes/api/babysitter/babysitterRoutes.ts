7349ca47d26caa73862835cdb066565acf658bc5
EliGandin <git>
2024-06-09 09:19:54 +0300

    emoved comemnted code

--- server/routes/api/babysitter/babysitterRoutes.ts
+++ server/routes/api/babysitter/babysitterRoutes.ts
@@ -7,42 +7,6 @@ const babysitterRouter = Router();
 
 const handler = new Handler();
 
-// babysitterRouter.get(
-//   "/:babysitterId",
-//   [param("babysitterId").notEmpty().isNumeric().withMessage("Invalid Input")],
-//   async (req: Request, res: Response) => {
-//     try {
-//       const fieldValidationResult = validationResult(req);
-//       if (!fieldValidationResult.isEmpty()) {
-//         return res.status(400).json({
-//           message: fieldValidationResult
-//             .array()
-//             .map((item) => item.msg)
-//             .join(" "),
-//         });
-//       }
-
-//       const { babysitterId } = req.params;
-
-//       const validation = await handler.userValidation(Number(babysitterId));
-//       if (!validation.isValid) {
-//         return res.status(400).json({ error: validation.message });
-//       }
-
-//       const numOfViews = await handler.numOfViews(Number(babysitterId));
-
-//       res.status(200).json({ numOfViews });
-//     } catch (e) {
-//       console.log(
-//         `Error message: ${req.body.id}: ${(e as Error).message}\n${
-//           (e as Error).stack
-//         }`
-//       );
-//       return res.status(500).end();
-//     }
-//   }
-// );
-
 babysitterRouter.get(
   "/interactions/:id",
   [param("id").notEmpty().isNumeric().withMessage("Invalid Input")],
