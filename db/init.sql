d34fd32cdd2581fb1e219d7a1e0f1612eca22a1b
ido123ziv <52109442+ido123ziv@users.noreply.github.com>
2024-06-13 11:48:04 +0300

    populate request for contact requests (#75)
    * populate request for contact requests
    
    * typo
    
    * pr comment

--- db/init.sql
+++ db/init.sql
@@ -131,8 +131,12 @@ CREATE table IF NOT EXISTS contact_requests (
 
 INSERT INTO contact_requests (request_status, user_name, user_email, message_title, user_message)
 VALUES
-('new', 'Nahum', 'nahum@test.com', 'Nahums Title', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                                                    nisi ut aliquip ex ea commodo consequat.'),
-('working-on', 'Dana', 'Dana@test.com', 'Thanks', 'Hi, I want to express my gratitude for the excellent service you offer on this platform.');
+('new', 'Nahum', 'nahum@test.com', 'Review', 'Does this app works for men as well?'),
+('working-on', 'Dana', 'Dana@test.com', 'Thanks', 'Hi, I want to express my gratitude for the excellent service you offer on this platform.'),
+('seen','Robbi','robbi@test.com', 'Is your site safe?', 'Hi, how do I know your site safe and my data will not be leaked'),
+('new','Dilen','dilen@test.com', 'Spontaneous', 'Can I find a babysitter spontaneously?'),
+('seen','Ivgeny','ivgeny@test.com', 'Languages', 'Which languages are available in this site?'),
+('working-on','Shoshi','shoshi@test.com', 'New Region', 'When will you be available in Beer Sheva?'),
+('done','Yuval','yuval@test.com', 'exposer', 'I feel like I do not see any request on my profile, what should I do'),
+('new','Eran', 'eran@example.com', 'new user', 'I want to join this site I heard a lot about it!'),
+('new','Gili', 'gilig@example.com', 'time to market', 'I want to add my friends here this would be great for them!');
