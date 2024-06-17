f2d9ee8eee34bf0487a9c4b7c7a590efedf4263d
ido.ziv <ido.ziv@kaltura.com>
2024-06-17 20:43:14 +0300

    tests with multi port

--- test/unit-test.js
+++ test/unit-test.js
@@ -1,21 +1,22 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
+const APP_PORT = process.env.APP_PORT || 3000;
 
 const outputFile = 'output.txt';
 const postUrlsMap = {
-    contact: "http://localhost:3000/contact",
-    login: "http://localhost:3000/login",
-    signupBabysitter: "http://localhost:3000/signup/babysitters",
-    signupParent: "http://localhost:3000/signup/parents",
-    recommendationText: "http://localhost:3000/api/recommendations/:id"
+    contact: `http://localhost:${APP_PORT}/contact`,
+    login: `http://localhost:${APP_PORT}/login`,
+    signupBabysitter: `http://localhost:${APP_PORT}/signup/babysitters`,
+    signupParent: `http://localhost:${APP_PORT}/signup/parents`,
+    recommendationText: `http://localhost:${APP_PORT}/api/recommendations/:id`
 };
 
 const putUrlsMap = {
-    signupBabysitter: "http://localhost:3000/api/profile/babysitter/update/:id",
-    signupParent: "http://localhost:3000/api/profile/parent/update/:id",
-    deleteBabysitter: "http://localhost:3000/api/delete/babysitter/:id",
-    deleteParent: "http://localhost:3000/api/delete/parent/:id",
-    moderatorRequest: "http://localhost:3000/api/moderator/editContactRequestStatus/:id"
+    signupBabysitter: `http://localhost:${APP_PORT}/api/profile/babysitter/update/:id`,
+    signupParent: `http://localhost:${APP_PORT}/api/profile/parent/update/:id`,
+    deleteBabysitter: `http://localhost:${APP_PORT}/api/delete/babysitter/:id`,
+    deleteParent: `http://localhost:${APP_PORT}/api/delete/parent/:id`,
+    moderatorRequest: `http://localhost:${APP_PORT}/api/moderator/editContactRequestStatus/:id`
 }
 const openBugs = {
     "PUT": {
@@ -78,8 +79,8 @@ const schemas = {
     // }
 };
 async function getBabysitterCount(){
-    const responseAll = await fetch('http://localhost:3000/api/moderator/allUsers');
-    const responseBaby = await fetch('http://localhost:3000/api/parents/allBabysitters');
+    const responseAll = await fetch(`http://localhost:${APP_PORT}/api/moderator/allUsers`);
+    const responseBaby = await fetch(`http://localhost:${APP_PORT}/api/parents/allBabysitters`);
     const allUsers = await responseAll.json();
     const babysitters = await responseBaby.json();
     
