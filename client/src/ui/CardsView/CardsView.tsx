6f5093b41cf51f7a9bb16dfa19678bd352c7ec43
EliGandin <git>
2024-06-17 10:33:47 +0300

    fixed typo

--- client/src/ui/CardsView/CardsView.tsx
+++ client/src/ui/CardsView/CardsView.tsx
@@ -1,65 +1,88 @@
-import {Card, Label, Rating} from "semantic-ui-react";
+import { Card, Label, Rating } from "semantic-ui-react";
 
 import styles from "./CardsView.module.css";
-import {useState} from "react";
+import { useState } from "react";
 import ModalView from "../ModalView/ModalView";
-import {CardsData, CardsDataArr} from "./CardViewProps";
+import { CardsData, CardsDataArr } from "./CardViewProps";
 
-const CardsView = ({data, screen}: CardsDataArr) => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [card, setCard] = useState<CardsData | undefined>(undefined);
+const CardsView = ({ data, screen }: CardsDataArr) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [card, setCard] = useState<CardsData | undefined>(undefined);
 
-    const handleClick = (element: CardsData) => {
-        setCard(element);
-        setIsOpen(() => !isOpen);
-    };
+  const handleClick = (element: CardsData) => {
+    setCard(element);
+    setIsOpen(() => !isOpen);
+  };
 
-    return (
-        <>
-            <div className={styles.cardContainer}>
-                {data?.map((element: CardsData) => {
-                    const labels = <div>
-                        {element?.contacted ? <Label as='a' color='green' size='tiny' tag>Contacted</Label> : null}
-                        {element?.workedWith ? <Label as='a' color='orange' size='tiny' tag>Worked With</Label> : null}
-                    </div>
-
-                    const description = <div>
-                        {labels}
-                        <br />
-                        {element.comments}
-                    </div>
-
-                    const rating =
-                        <Rating icon='star' rating={element.rating} maxRating={5} disabled></Rating>
-
-                        return (<Card
-                            key={element.email}
-                            className={styles.card}
-                            image={
-                                element.imageString || element.role === "parents"
-                                    ? "/baby.svg"
-                                    : "/babysitter.svg"
-                            }
-                            header={element.name}
-                            meta={element?.distance ? `Distance: ${element.distance} km` : element.role}
-                            description={description}
-                            extra={rating}
-                            onClick={(_e) => handleClick(element)}
-                        />)
-                    }
-                )}
+  return (
+    <>
+      <div className={styles.cardContainer}>
+        {data?.map((element: CardsData) => {
+          const labels = (
+            <div>
+              {element?.contacted ? (
+                <Label as="a" color="green" size="tiny" tag>
+                  Contacted
+                </Label>
+              ) : null}
+              {element?.workedWith ? (
+                <Label as="a" color="orange" size="tiny" tag>
+                  Worked With
+                </Label>
+              ) : null}
             </div>
+          );
 
-            {isOpen && (
-                <ModalView
-                    isOpen={isOpen}
-                    setIsOpen={setIsOpen}
-                    card={card}
-                    screen={screen}
-                />
-            )}
-        </>
-    );
+          const description = (
+            <div>
+              {labels}
+              <br />
+              {element.comments}
+            </div>
+          );
+
+          const rating = (
+            <Rating
+              icon="star"
+              rating={element.rating}
+              maxRating={5}
+              disabled
+            ></Rating>
+          );
+
+          return (
+            <Card
+              key={element.email}
+              className={styles.card}
+              image={
+                element.imageString || element.role === "parent"
+                  ? "/baby.svg"
+                  : "/babysitter.svg"
+              }
+              header={element.name}
+              meta={
+                element?.distance
+                  ? `Distance: ${element.distance} km`
+                  : element.role
+              }
+              description={description}
+              extra={rating}
+              onClick={(_e) => handleClick(element)}
+            />
+          );
+        })}
+      </div>
+
+      {isOpen && (
+        <ModalView
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          card={card}
+          screen={screen}
+        />
+      )}
+    </>
+  );
 };
 
 export default CardsView;
